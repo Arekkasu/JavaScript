f38e5de5ef1ceed09d8f2dd57792fd878954e1ae
Arekkasu <alexanderlozada230@gmail.com>
2023-09-09 18:52:50 -0500

    caracteristicas

--- ECMAScript 6/Iterator&iterable.js
+++ ECMAScript 6/Iterator&iterable.js
@@ -23,16 +23,16 @@ while (!next.done){
 	next = iterador.next()
 }
 let text = ` ESTOS SON LAS ESTRUCTURAS DE DATOS QUE ADMITE
-Arrays: Una colección ordenada de elementos, accesibles por un índice numérico.
-Strings: Secuencias de caracteres. Cada carácter tiene un índice numérico y es iterable.
-Sets: Colecciones de valores únicos.
-Maps: Colecciones de pares clave-valor.
-Typed Arrays: Arrays de datos binarios con un tipo específico de elementos (como enteros de 8 bits, enteros sin signo de 32 bits, etc.).
-NodeLists: Listas de nodos de un documento HTML.
-Arguments Object: El objeto arguments que está disponible dentro de una función y contiene los argumentos pasados a esa función.
-Promises: Representan un valor que puede estar disponible ahora, en el futuro, o nunca.
-Iterables personalizados: Puedes crear tus propios objetos iterables implementando el método [Symbol.iterator].
-Generators: Funciones especiales que pueden ser pausadas y reanudadas.
+1. Arrays: Una colección ordenada de elementos, accesibles por un índice numérico.
+2. Strings: Secuencias de caracteres. Cada carácter tiene un índice numérico y es iterable.
+3. Sets: Colecciones de valores únicos.
+4. Maps: Colecciones de pares clave-valor.
+5. Typed Arrays: Arrays de datos binarios con un tipo específico de elementos (como enteros de 8 bits, enteros sin signo de 32 bits, etc.).
+6. NodeLists: Listas de nodos de un documento HTML.
+7. Arguments Object: El objeto arguments que está disponible dentro de una función y contiene los argumentos pasados a esa función.
+8. Promises: Representan un valor que puede estar disponible ahora, en el futuro, o nunca.
+9. Iterables personalizados: Puedes crear tus propios objetos iterables implementando el método [Symbol.iterator].
+10. Generators: Funciones especiales que pueden ser pausadas y reanudadas.
 `;
 
 console.log(text)
